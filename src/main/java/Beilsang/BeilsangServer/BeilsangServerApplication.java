f9f7e19fe108d98f7b70b666e3ef04826f8ec1e8
Chan-Heee <cksgml019@naver.com>
2024-01-16 20:59:48 +0900

    init settings

---
+++ src/main/java/Beilsang/BeilsangServer/BeilsangServerApplication.java
@@ -0,0 +1,13 @@
+package Beilsang.BeilsangServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeilsangServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeilsangServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Beilsang/BeilsangServer/BeilsangServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Beilsang.BeilsangServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeilsangServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
