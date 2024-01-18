45176abce8b52ce35448b32377d2cee9ee5d5d74
Chan-Heee <cksgml019@naver.com>
2024-01-18 22:47:12 +0900

    [CI/CD] Dockerfile 및 테스트용 컨트롤러 구현

---
+++ src/main/java/com/BeilsangServer/global/TestController.java
@@ -0,0 +1,13 @@
+package com.BeilsangServer.global;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("테스트 성공");
+    }
+}
