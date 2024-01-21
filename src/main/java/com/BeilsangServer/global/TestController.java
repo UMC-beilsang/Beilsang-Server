56365283bd4b76e6a31ca92d7d50d42c3809bb86
Chan-Heee <cksgml019@naver.com>
2024-01-21 22:32:01 +0900

    [REFAC] ci/cd yml 파일 수정 및 테스트 코드 수정

--- src/main/java/com/BeilsangServer/global/TestController.java
+++ src/main/java/com/BeilsangServer/global/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<String> test(){
-        return ResponseEntity.ok("테스트 성공");
+        return ResponseEntity.ok("CI/CD 테스트 성공");
     }
 }
