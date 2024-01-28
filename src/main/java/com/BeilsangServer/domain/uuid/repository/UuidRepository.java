4093a34f019b9a170c1474be1076cd29c731c630
JJadeYoon <yess9904@naver.com>
2024-01-28 21:47:36 +0900

    [FEAT] AWS S3 세팅

---
+++ src/main/java/com/BeilsangServer/domain/uuid/repository/UuidRepository.java
@@ -0,0 +1,9 @@
+package com.BeilsangServer.domain.uuid.repository;
+
+import com.BeilsangServer.domain.uuid.entity.Uuid;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UuidRepository extends JpaRepository<Uuid, Long> {
+}
