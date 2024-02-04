a3109d7c254a9324b94678aa55b67fca12c1019d
Chan-Heee <cksgml019@naver.com>
2024-02-04 20:48:58 +0900

    [FEAT] FirebaseConfig 구현

---
+++ src/main/java/com/BeilsangServer/domain/notification/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.BeilsangServer.domain.notification;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    private final ClassPathResource firebaseResource = new ClassPathResource(
+            "firebase/beilsang-d4e6d-firebase-adminsdk-i3izy-860a771b1b.json");
+
+    @Bean
+    //Firebase앱 초기화
+    FirebaseApp firebaseApp() throws IOException {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(
+                        firebaseResource.getInputStream()))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        return FirebaseMessaging.getInstance(firebaseApp());
+    }
+}
