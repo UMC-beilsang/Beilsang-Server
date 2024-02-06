4f7ab1946a123eba6e0765ce1123484e61829084
topsilverr <wltkddms9909@naver.com>
2024-02-06 23:44:44 +0900

    [REFACT] swagger 설명 추가

--- src/main/java/com/BeilsangServer/domain/feed/FeedController.java
+++ src/main/java/com/BeilsangServer/domain/feed/FeedController.java
@@ -5,6 +5,8 @@ import com.BeilsangServer.domain.feed.dto.FeedDTO;
 import com.BeilsangServer.domain.feed.service.FeedService;
 import com.BeilsangServer.global.common.apiResponse.ApiResponse;
 import com.BeilsangServer.global.common.apiResponse.ApiResponseStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -24,6 +26,10 @@ public class FeedController {
     private final FeedService feedService;
 
     @PostMapping(value = "/feeds/{challengeId}",consumes = MULTIPART_FORM_DATA, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "피드 생성 API", description = "챌린지 인증을 통해 피드를 생성하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<FeedDTO> createFeed(
             @RequestPart MultipartFile file,
             @RequestPart String review,
@@ -34,6 +40,10 @@ public class FeedController {
     }
 
     @GetMapping("/feeds/guide/{challengeId}")
+    @Operation(summary = "인증 가이드 조회 API", description = "챌린지 인증 시 인증 가이드를 조회하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<ChallengeResponseDTO.ChallengeGuide> getGuide(
             @PathVariable(name = "challengeId") Long challengeId
     ){
@@ -42,6 +52,10 @@ public class FeedController {
     }
 
     @GetMapping("/feeds/{feedId}")
+    @Operation(summary = "특정 피드 정보 조회 API", description = "사용자가 선택한 피드의 정보를 조회하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<FeedDTO> getFeed(
             @PathVariable(name = "feedId") Long feedId
     ){
@@ -50,6 +64,10 @@ public class FeedController {
     }
 
     @GetMapping("/feeds/search")
+    @Operation(summary = "챌린지 제목으로 피드 검색 API", description = "검색어가 제목에 포함된 챌린지와 관련된 피드를 검색하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<FeedDTO.previewFeedListDto> searchFeed(
             @RequestParam("name") String name
     ){
@@ -59,6 +77,10 @@ public class FeedController {
     }
 
     @PostMapping("/feeds/{feedId}/likes")
+    @Operation(summary = "피드 좋아요 API", description = "사용자가 원하는 피드에 좋아요를 누르는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<Long> feedLike(
             @PathVariable(name = "feedId") Long feedId
     ){
@@ -67,6 +89,10 @@ public class FeedController {
     }
 
     @DeleteMapping("/feeds/{feedId}/likes")
+    @Operation(summary = "피드 좋아요 취소 API", description = "좋아요를 취소하는 삭제하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<Long> feedUnLike(
             @PathVariable(name = "feedId") Long feedId
     ){
@@ -76,6 +102,10 @@ public class FeedController {
     }
 
     @GetMapping("/feeds/category/{category}")
+    @Operation(summary = "카테고리로 필터링한 피드 조회 API", description = "선택된 카테고리에 해당하는 피드를 조회하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<FeedDTO.previewFeedListDto> getFeedByCategory(
             @PathVariable(name = "category") String category
     ){
@@ -85,6 +115,10 @@ public class FeedController {
     }
 
     @GetMapping("/feeds/{status}/{category}")
+    @Operation(summary = "카테고리와 상태로 필터링한 내 피드 조회 API", description = "나의 피드에 대해 카테고리와 챌린지 상태로 필터링하여 피드를 조회하는 API 입니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",description = "성공")
+    })
     public ApiResponse<List<FeedDTO>> getFeedByStatusAndCategory(
             @PathVariable(name = "status") String status,
             @PathVariable(name = "category") String category
