27cc9a93c3b818fd535deaa72e8c37443b9d4f9d
fewolee <chulwoo1304@gmail.com>
2024-02-09 00:51:52 +0900

    [REFAC]전역 예외 처리 수정,회원탈퇴 API 추가

--- src/main/java/com/BeilsangServer/global/jwt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/BeilsangServer/global/jwt/exception/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(CustomException.class)
