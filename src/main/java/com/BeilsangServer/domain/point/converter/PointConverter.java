1402f6e7bae49130b467085dd55344abaa337e9f
JJadeYoon <yess9904@naver.com>
2024-02-14 14:42:18 +0900

    [REFAC] PointLog converter 수정

--- src/main/java/com/BeilsangServer/domain/point/converter/PointConverter.java
+++ src/main/java/com/BeilsangServer/domain/point/converter/PointConverter.java
@@ -13,7 +13,7 @@ public class PointConverter {
         List<PointResponseDTO.pointLogDTO> points = pointLogs.stream().map(pointLog -> PointResponseDTO.pointLogDTO.builder()
                 .id(pointLog.getId())
                 .value(pointLog.getValue())
-                .name(pointLog.getName())
+                .name(pointLog.getPointName().getDescription())
                 .period(pointLog.getPeriod())
                 .date(pointLog.getCreatedAt().toLocalDate())
                 .status(pointLog.getStatus())
